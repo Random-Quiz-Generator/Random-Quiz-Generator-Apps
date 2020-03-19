0ba3d7dd50d680ffd8adb3ad050daabf9530fa6e
Ridza Adhandra <andradhandra@gmail.com>
2020-03-19 15:29:53 +0700

    Add question logic

--- server/bin/http.js
+++ server/bin/http.js
@@ -1,10 +1,25 @@
 const app = require('../app')
 const http = require('http')
 const server = http.createServer(app)
-const socketio = require('socket.io')(server)
+const io = require('socket.io')(server)
+const fs = require('fs')
 const port = 3000
 
-socketio.on('connection', socket => {
+const questions = JSON.parse(fs.readFileSync('questions.json', 'utf-8'))
+
+//nanti dipanggil ke function socket yang get question
+function getQuestion () {
+  let counter = 0
+  if (counter > 10) {
+    counter += 1
+    const random = Math.floor(Math.random() * Math.floor(questions.length))
+    return questions.splice(random, 1)
+  } else {
+    return null
+  }
+}
+
+io.on('connection', socket => {
   console.log('user connected')
 })
 
