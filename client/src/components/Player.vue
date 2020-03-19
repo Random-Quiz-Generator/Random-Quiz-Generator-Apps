a64d6ad8be97e2c05109c95bf8cc7de20c52ee2a
prmzk <adam.primarizki@gmail.com>
2020-03-19 14:08:30 +0700

    home, lobby 1/2

---
+++ client/src/components/Player.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-row class="p-2 mr-3 ml-3 mt-2 player">{{name}}</b-row>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style>
+  .player {
+    border: 1px solid rgb(219, 219, 219);
+    border-radius: 20px;
+  }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,57 +1,65 @@
 <template>
-  <div class="home d-flex">
-    <b-container class="home-container d-flex flex-column align-items-center">
-      <h1 class="display-3 mt-5 pt-5">Welcome to</h1>
-      <h1 class="display-3 bounce infinite animated">Random Quiz</h1>
-      <small>Some are funny. Some are mindblowing.</small>
-      <b-form class="pt-5 d-flex flex-column align-items-start">
-        <b-form-group>
-          <b-form-input
-            v-model= "name"
-            type="text"
-            required
-            placeholder="Enter your name"
-            class="form-input"
-          ></b-form-input>
-      </b-form-group>
-      <b-button type="submit" variant="primary" class="join">Join the fun now</b-button>
-      </b-form>
-    </b-container>
-  </div>
+    <div class="room d-flex">
+      <b-container class="room-container d-flex flex-column align-items-center shadow">
+        <h1 class="display-3 mt-3">Welcome!</h1>
+        <small>Click start when everyone ready!</small>
+        <b-row class="lobby m-3">
+          <b-col cols="4 players-container p-2">
+           <h1 class="display-6 mt-3 player-title">Player joined: </h1>
+           <small>Minimum player: 2</small>
+           <player v-for="(player, i) in players" :key="i" :name="players[i]"></player>
+          </b-col>
+          <b-col cols="8 how-to p-2">howto</b-col>
+        </b-row>
+      </b-container>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Player from '../components/Player'
 export default {
   name: 'Home',
   components: {
+    Player
   },
   data () {
     return {
-      name: ''
+      players: [
+        'Adam',
+        'Ridza',
+        'Haidar',
+        'Yoko'
+      ]
     }
   }
 }
 </script>
 
 <style>
-  .home{
+  .room{
     background-color: rgb(245, 243, 243);
   }
-  .home-container {
-    background-image: url('../assets/bg.jpg');
+  .room-container {
+    background-color: rgb(255, 255, 255);
     height: 100vh;
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
   }
-  .form-input {
-    width: 29.5rem !important;
-    margin-top: 10rem;
+  .players-container {
+    border-right: 1px solid rgb(219, 219, 219);
   }
-  .join {
-    background-color: rgb(8, 77, 25) !important;
-    border: 0 !important;
+  .how-to {
+  }
+  .lobby{
+    height: 100vh;
+    border: 1px solid rgb(219, 219, 219);
+    width: 100%;
+    border-radius: 20px;
+  }
+  .player-title {
+    font-size: 30px;
+    font-weight: bold;
   }
 </style>
